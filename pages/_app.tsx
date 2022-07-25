d727e0203e16d1021015832ff2bd7307e2ffe617
siihyun <sihyun930222@gmail.com>
2022-07-25 23:02:24 +0900

    feat: test mock api 생성

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,11 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { setupMSW } from '../mocks';
+
+if (process.env.NODE_ENV === 'development') {
+  console.log('msw started');
+  setupMSW();
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
