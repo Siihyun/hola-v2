116bbb1ee663969b180440c2f5cadaa8b80c3ed5
siihyun <sihyun930222@gmail.com>
2022-07-30 01:02:06 +0900

    feat: main page 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from 'next';
 import useGetStudyList from '../domains/study/hooks/useGetStudyList';
 
-const Home: NextPage = () => {
+const Main: NextPage = () => {
   const { data, isLoading } = useGetStudyList();
   if (isLoading) return <div>loading...</div>;
   console.log(data);
-  return <div>hi</div>;
+  return <div>main page</div>;
 };
 
-export default Home;
+export default Main;
