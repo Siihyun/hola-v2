7f66879d363559544809a2f0c09c1ece8c50d37d
siihyun <sihyun930222@gmail.com>
2022-07-29 23:39:14 +0900

    fix: query key 수정

--- domains/study/hooks/useGetStudyDetail.ts
+++ domains/study/hooks/useGetStudyDetail.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import { studyRepository } from '../../../repository';
 
 const useStudyDetail = (id: string) => {
-  return useQuery(['studyList'], () => studyRepository.getDetail(id));
+  return useQuery(['studyDetail'], () => studyRepository.getDetail(id));
 };
 
 export default useStudyDetail;
